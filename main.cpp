9b3b65523b935c5b484c6d3fa5514f5a34298892
Nazar Glukhov <nazar.glukhov@gmail.com>
2023-09-11 00:08:30 +0300

    workflows added

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "lib.h"
 
-int main (int, char **) {
+int main (int, char**) {
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
